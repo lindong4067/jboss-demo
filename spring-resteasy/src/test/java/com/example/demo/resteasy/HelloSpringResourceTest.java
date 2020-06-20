e14fead793b86ff3a270740c81f7f33d9f417517
lindong zhao <lindong4067@sina.com>
2020-06-20 16:26:26 +0800

    add spring resteasy test

---
+++ spring-resteasy/src/test/java/com/example/demo/resteasy/HelloSpringResourceTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.resteasy;
+
+import org.junit.Test;
+
+public class HelloSpringResourceTest {
+
+    @Test
+    public void testHelloSpringResource() throws Exception {
+
+    }
+
+    @Test
+    public void testLocatingResource() throws Exception {
+    }
+}
