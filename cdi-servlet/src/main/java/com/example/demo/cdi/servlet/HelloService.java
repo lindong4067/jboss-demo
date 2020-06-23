61361728481926d96e05bfbd38c73e0d2f097937
Lindong Zhao <lindong.zhao@nokia-sbell.com>
2020-06-23 14:41:22 +0800

    add cdi servlet

--- cdi-servlet/src/main/java/com/example/demo/cdi/servlet/HelloService.java
+++ cdi-servlet/src/main/java/com/example/demo/cdi/servlet/HelloService.java
@@ -1,4 +1,10 @@
 package com.example.demo.cdi.servlet;
 
+/**
+ * A simple CDI service which is able to say hello to someone
+ */
 public class HelloService {
+    String createHelloMessage(String name) {
+        return "Hello " + name + "!";
+    }
 }
--- cdi-servlet/src/main/java/com/example/demo/cdi/servlet/HelloServiceServlet.java
+++ cdi-servlet/src/main/java/com/example/demo/cdi/servlet/HelloServiceServlet.java
@@ -1,4 +1,32 @@
 package com.example.demo.cdi.servlet;
 
-public class HelloServiceServlet {
+import jakarta.inject.Inject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/helloworld")
+public class HelloServiceServlet extends HttpServlet {
+
+    static String PAGE_HEADER = "<html><head><title>helloworld</title></head><body>";
+
+    static String PAGE_FOOTER = "</body></html>";
+
+    @Inject
+    HelloService helloService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.println(PAGE_HEADER);
+        writer.println("<h1>" + helloService.createHelloMessage("World") + "</h1>");
+        writer.println(PAGE_FOOTER);
+        writer.close();
+    }
 }
